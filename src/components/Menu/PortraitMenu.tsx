cbea570490153a780fcc5291da728c2b73d4096c
Ezequiel Amin <ezequielamin@outlook.com>
2023-12-24 18:00:35 -0300

    refactor(menu): add logo and change layout of portrait menu

--- src/components/Menu/PortraitMenu.tsx
+++ src/components/Menu/PortraitMenu.tsx
@@ -24,25 +24,29 @@ const PortraitMenu = (): JSX.Element => {
         <Drawer.Content className="bg-zinc-700 flex flex-col rounded-t-[10px] mt-24 fixed bottom-0 left-0 right-0 z-50">
           <div className="bg-white dark:bg-zinc-700 duration-300 rounded-t-[10px] flex-1 p-4 pb-0">
             <div className="mx-auto w-12 h-1 flex-shrink-0 rounded-full bg-zinc-300 mb-4" />
-            <div className="max-w-md">
-              <Drawer.Title className="mb-4">
-                {/* Avatar */}
-                <h2 className="text-center text-xl">
-                  Hola, <span className="font-bold">{user?.name}</span>
-                </h2>
-              </Drawer.Title>
+            <section className="max-w-md mb-4 text-center">
+              <img
+                alt="Logo Custodia de Archivos"
+                className="inline mb-4"
+                src="/img/logo.png"
+                width={150}
+              />
+              {/* Avatar */}
+              <h2 className="text-xl">
+                Hola, <span className="font-bold">{user?.name}</span>
+              </h2>
+            </section>
+            <div className="bg-white border-t border-zinc-200 dark:bg-zinc-700 duration-400 mt-auto py-4 -mb-5">
+              {/* Botones de los Modulos */}
+              {routes.map((el) => (
+                <MenuModuleButton open el={el} key={el.id} />
+              ))}
             </div>
             <div className="border-t border-zinc-200 py-4 flex gap-4 justify-center max-w-md mx-auto">
               {/* Boton para Cerrar Sesion */}
               <LogoutButton open className="px-4 flex-1 mb-0" />
               {/* Cambiar tema */}
               <ThemeTogglerButton />
-            </div>
-            <div className="bg-white border-t border-zinc-200 dark:bg-zinc-700 duration-400 mt-auto py-4">
-              {/* Botones de los Modulos */}
-              {routes.map((el) => (
-                <MenuModuleButton open el={el} key={el.id} />
-              ))}
             </div>
           </div>
         </Drawer.Content>
