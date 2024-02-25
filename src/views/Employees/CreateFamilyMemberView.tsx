60a05833756fe5cac4c0c116a4a5a9811d3b0c1e
Ezequiel Amin <ezequielamin@outlook.com>
2024-02-25 20:39:20 -0300

    feat: add cancel button to family member view

--- src/views/Employees/CreateFamilyMemberView.tsx
+++ src/views/Employees/CreateFamilyMemberView.tsx
@@ -1,10 +1,19 @@
+import { useParams } from 'react-router-dom';
+
 import Title from '@/components/Common/Title';
 import FamilyMemberForm from '@/components/Views/Employees/V5_Family/FamilyMemberForm';
 
 const EmployeeFamilyView = () => {
+  const { id: employeeId } = useParams();
+
   return (
     <>
-      <Title title="Agregar familiar" />
+      <Title
+        buttonClassName="bg-gray-500 hover:bg-gray-600 dark:bg-gray-600 dark:hover:bg-gray-700"
+        buttonText="Cancelar"
+        href={`/employees/${employeeId}/personal`}
+        title="Agregar familiar"
+      />
       <FamilyMemberForm />
     </>
   );
